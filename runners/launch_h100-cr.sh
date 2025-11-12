39fdbcece60facc9a138f14d1cedc9f3e17e61f3
Cam Quilici <cjquilici@gmail.com>
2025-11-12 16:35:43 -0600

    cleaning up

--- runners/launch_h100-cr.sh
+++ runners/launch_h100-cr.sh
@@ -4,7 +4,6 @@ HF_HUB_CACHE_MOUNT="/home/ubuntu/hf_hub_cache/"
 PORT=8888
 
 server_name="bmk-server"
-# client_name="bmk-client"
 
 set -x
 docker run --rm --network=host --name=$server_name \
@@ -16,39 +15,3 @@ docker run --rm --network=host --name=$server_name \
 --entrypoint=/bin/bash \
 $IMAGE \
 benchmarks/"${EXP_NAME%%_*}_${PRECISION}_h100_docker.sh"
-
-# set +x
-# while IFS= read -r line; do
-#     printf '%s\n' "$line"
-#     if [[ "$line" =~ Application\ startup\ complete ]]; then
-#         break
-#     fi
-# done < <(docker logs -f --tail=0 $server_name 2>&1)
-
-# if ! docker ps --format "{{.Names}}" | grep -q "$server_name"; then
-#     echo "Server container launch failed."
-#     exit 1
-# fi
-
-# git clone https://github.com/kimbochen/bench_serving.git
-
-# set -x
-# docker run --rm --network=host --name=$client_name \
-# -v $GITHUB_WORKSPACE:/workspace/ -w /workspace/ \
-# -e HF_TOKEN -e PYTHONPYCACHEPREFIX=/tmp/pycache/ \
-# --entrypoint=/bin/bash \
-# $IMAGE \
-# -lc "pip install -q datasets pandas && \
-# python3 bench_serving/benchmark_serving.py \
-# --model=$MODEL \
-# --backend=vllm \
-# --base-url=\"http://localhost:$PORT\" \
-# --dataset-name=random \
-# --random-input-len=$ISL --random-output-len=$OSL --random-range-ratio=$RANDOM_RANGE_RATIO \
-# --num-prompts=$(( $CONC * 10 )) --max-concurrency=$CONC \
-# --request-rate=inf --ignore-eos \
-# --save-result --percentile-metrics='ttft,tpot,itl,e2el' \
-# --result-dir=/workspace/ \
-# --result-filename=$RESULT_FILENAME.json"
-
-# docker stop $server_name
